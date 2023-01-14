bc2dd82cf1e5fe11c121ab11804c28afe4a2f82a
Kamilovkz <001kamilov@gmail.com>
2023-01-14 13:21:30 +0600

    Final fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter
-
+import password
 import customtkinter as CTk
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 from PIL import Image
@@ -12,22 +12,22 @@ class App(CTk.CTk):
         self.title("Password Generator")
         self.resizable(False, False)
 
-        self.logo = CTk.CTkImage(dark_image=Image.open("img.png"), size=(460, 150))
+        self.logo = CTk.CTkImage(dark_image=Image.open("img.png"), size=(460, 160))
         self.logo_label = CTk.CTkLabel(master=self, text="", image=self.logo)
         self.logo_label.grid(row=0, column=0)
 
         self.password_frame = CTk.CTkFrame(master=self, fg_color="transparent")
-        self.password_frame.grid(row=1, column=0, padx=(20, 20), sticky="nsew")
+        self.password_frame.grid(row=1, column=0, padx=(20, 20), pady=(10, 10), sticky="nsew")
 
         self.entry_password = CTk.CTkEntry(master=self.password_frame, width=300)
-        self.entry_password.grid(row=0, column=0, padx=(0, 20))
+        self.entry_password.grid(row=0, column=0, padx=(10, 20))
 
         self.btn_generator = CTk.CTkButton(master=self.password_frame, text="Generate", width=100,
                                            command=self.set_password)
         self.btn_generator.grid(row=0, column=1)
 
         self.settings_frame = CTk.CTkFrame(master=self)
-        self.settings_frame.grid(row=2, column=0, padx=(20,20), pady=(20, 0), sticky='nsew')
+        self.settings_frame.grid(row=2, column=0, padx=(20, 20), pady=(10, 0), sticky='nsew')
 
         self.password_length_slider = CTk.CTkSlider(master=self.settings_frame, from_=0, to=100, number_of_steps=100,
                                                     command=self.slider_event)
@@ -61,14 +61,28 @@ class App(CTk.CTk):
                                                              command=self.change_appearance_mode_event)
         self.appearance_mode_option_menu.grid(row=3, column=0, columnspan=4, pady=(10, 10))
 
+        self.password_length_slider.set(12)
+        self.password_length_entry.insert(0, 12)
+        self.appearance_mode_option_menu.set("System")
+
     def change_appearance_mode_event(self, new_appearance_mode):
         CTk.set_appearance_mode(new_appearance_mode)
 
     def slider_event(self, value):
         self.password_length_entry.delete(0, "end")
         self.password_length_entry.insert(0, int(value))
+
+    def get_charaterst(self):
+        chars = "".join(self.cb_digits_var.get() + self.cb_lower_var.get() + self.cb_upper_var.get() + self.cb_symbol_var.get())
+        return chars
+
     def set_password(self):
-        pass
+        try:
+            self.entry_password.delete(0, "end")
+            self.entry_password.insert(0, password.create_new(length=int(self.password_length_slider.get()),
+                                                              characters=self.get_charaterst()))
+        except:
+            pass
 
 if __name__ == '__main__':
     app = App()
